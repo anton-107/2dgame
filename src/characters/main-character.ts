9e800561b9c8f81e880e86736c0786028cae69e1
anton-107 <226657+anton-107@users.noreply.github.com>
2025-01-21 11:21:30 +0100

    refactor: move main character logic into a separate class

---
+++ src/characters/main-character.ts
@@ -0,0 +1,92 @@
+import * as Phaser from "phaser";
+
+type MainCharacterMovement =
+  | "stopped"
+  | "walking-up"
+  | "walking-down"
+  | "walking-left"
+  | "walking-right";
+
+export default class MainCharacter {
+  private sprite: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody | undefined;
+  private speed = 200;
+  private currentMovement: MainCharacterMovement = "stopped";
+
+  public addToScene(scene: Phaser.Scene) {
+    // Create the character sprite
+    this.sprite = scene.physics.add.sprite(400, 300, "guy", 1);
+
+    // Create input keys
+    if (!scene.input) {
+      throw "Input should be defined";
+    }
+    if (!scene.input.keyboard) {
+      throw "Keyboard input should be defined";
+    }
+
+    this.createAnimations(scene);
+  }
+
+  public setMovement(movement: MainCharacterMovement) {
+    this.currentMovement = movement;
+  }
+
+  private createAnimations(scene: Phaser.Scene) {
+    scene.anims.create({
+      key: "up",
+      frames: scene.anims.generateFrameNumbers("guy", {
+        frames: [12, 13, 14, 15],
+      }),
+      frameRate: 8,
+      repeat: -1,
+    });
+
+    scene.anims.create({
+      key: "down",
+      frames: scene.anims.generateFrameNumbers("guy", { frames: [0, 1, 2, 3] }),
+      frameRate: 8,
+      repeat: -1,
+    });
+
+    scene.anims.create({
+      key: "left",
+      frames: scene.anims.generateFrameNumbers("guy", {
+        frames: [8, 9, 10, 11],
+      }),
+      frameRate: 8,
+      repeat: -1,
+    });
+
+    scene.anims.create({
+      key: "right",
+      frames: scene.anims.generateFrameNumbers("guy", { frames: [4, 5, 6, 7] }),
+      frameRate: 8,
+      repeat: -1,
+    });
+  }
+
+  public update() {
+    if (!this.sprite) {
+      return;
+    }
+    // Reset character's velocity
+    this.sprite.setVelocity(0);
+
+    // Move character based on arrow key inputs
+    if (this.currentMovement === "walking-up") {
+      this.sprite.setVelocityY(-this.speed);
+      this.sprite.play("up", true);
+    } else if (this.currentMovement === "walking-down") {
+      this.sprite.setVelocityY(this.speed);
+      this.sprite.play("down", true);
+    } else if (this.currentMovement === "walking-left") {
+      this.sprite.setVelocityX(-this.speed);
+      this.sprite.play("left", true);
+    } else if (this.currentMovement === "walking-right") {
+      this.sprite.setVelocityX(this.speed);
+      this.sprite.play("right", true);
+    } else {
+      this.sprite.anims.stop();
+    }
+  }
+}
--- src/scenes/city-scene.ts
+++ src/scenes/city-scene.ts
@@ -1,82 +1,56 @@
 import * as Phaser from "phaser";
 
+import MainCharacter from "../characters/main-character";
+
 export default class CityScene extends Phaser.Scene {
-  private cursorsKeys: Phaser.Types.Input.Keyboard.CursorKeys;
-  private character: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  private character: MainCharacter | undefined;
+  private cursorsKeys: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+
   constructor() {
     super("city");
   }
+
   preload() {
     this.load.spritesheet("guy", "guy.png", {
       frameWidth: 16,
       frameHeight: 24,
     });
   }
-  create() {
-    // Create the character sprite
-    this.character = this.physics.add.sprite(400, 300, "guy", 1);
 
-    // Create animations for all directions
-    this.anims.create({
-      key: "up",
-      frames: this.anims.generateFrameNumbers("guy", { frames: [12, 13, 14, 15] }),
-      frameRate: 8,
-      repeat: -1
-    });
+  public create() {
+    this.character = new MainCharacter();
+    this.character.addToScene(this);
 
-    this.anims.create({
-      key: "down",
-      frames: this.anims.generateFrameNumbers("guy", { frames: [0, 1, 2, 3] }),
-      frameRate: 8,
-      repeat: -1
-    });
+    this.initializeCursorKeys();
+  }
 
-    this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("guy", { frames: [8, 9, 10, 11] }),
-      frameRate: 8,
-      repeat: -1
-    });
+  public update() {
+    if (!this.character) {
+      return;
+    }
+    if (this.cursorsKeys) {
+      if (this.cursorsKeys.up?.isDown) {
+        this.character.setMovement("walking-up");
+      } else if (this.cursorsKeys.down?.isDown) {
+        this.character.setMovement("walking-down");
+      } else if (this.cursorsKeys.left?.isDown) {
+        this.character.setMovement("walking-left");
+      } else if (this.cursorsKeys.right?.isDown) {
+        this.character.setMovement("walking-right");
+      } else {
+        this.character.setMovement("stopped");
+      }
+    }
+    this.character.update();
+  }
 
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("guy", { frames: [4, 5, 6, 7] }),
-      frameRate: 8,
-      repeat: -1
-    });
-
-    // Enable keyboard input
+  private initializeCursorKeys() {
     if (!this.input) {
-      throw "Input should be defined";
+      throw Error("Input is not defined");
     }
     if (!this.input.keyboard) {
-      throw "Keyboard input should be defined";
+      throw Error("Keyboard is not defined");
     }
     this.cursorsKeys = this.input.keyboard.createCursorKeys();
-  }
-  update() {
-    const speed = 200; // Adjust as needed
-
-    // Reset character's velocity
-    this.character.setVelocity(0);
-
-    // Move character based on arrow key inputs
-    if (this.cursorsKeys.up?.isDown) {
-      this.character.setVelocityY(-speed);
-      this.character.play("up", true);
-    } else if (this.cursorsKeys.down?.isDown) {
-      this.character.setVelocityY(speed);
-      this.character.play("down", true);
-    }
-
-    if (this.cursorsKeys.left?.isDown) {
-      this.character.setVelocityX(-speed);
-      this.character.play("left", true);
-    } else if (this.cursorsKeys.right?.isDown) {
-      this.character.setVelocityX(speed);
-      this.character.play("right", true);
-    } else {
-      this.character.anims.stop();
-    }
   }
 }
